bf343ca5c3f3e063aaef80e62e4ee6e2794e5cc1
vino <vino0908@outlook.com>
2021-05-23 21:02:50 +0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import React, {memo, useCallback, useMemo, useState} from "react";
-
+import React, {useEffect, useRef, useState} from "react";
 
 const App = () => {
     const [n, setN] = useState(0)
-    const [m] = useState(0)
+    const count = useRef(0)
     const addOne = () => {
         setN(i => i + 1)
     }
-    const onClickChild = useCallback(() => {
-        console.log(m)
-    }, [m])
-    /**
-     * 等价于下面的useMemo
-     */
-    // const onClickChild = useMemo(() => {
-    //     return () => {
-    //         console.log(m)
-    //     }
-    // }, [m])
+    useEffect(() => {
+        count.current += 1
+        console.log(count.current)
+    })
     return (
         <div>
             <span>{n}</span>
             <hr/>
             <button onClick={addOne}>+1</button>
-            <hr/>
-            <Child data={m} onClick={onClickChild}/>
         </div>
     )
 }
-const Child = memo((props) => {
-    console.log('child 执行了')
-    console.log('假设这里有大量代码')
-    return (
-        <div onClick={props.onClick}>child: {props.data}</div>
-    )
-})
-
 export default App
