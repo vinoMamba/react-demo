08bd19bb53a08e8eca194cbf0af1c623bbc8c50c
vino <vino0908@outlook.com>
2021-05-23 17:05:17 +0800

    reducer

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React, {useReducer} from "react";
 
-let _state = []
-let index = 0
-const myUseState = (initialValue) => {
-    const currentIndex = index
-    _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
-    const setState = (newValue) => {
-        _state[currentIndex] = newValue
-        render()
-    }
-    index += 1
-    return [_state[currentIndex], setState]
+/**
+ * useState 的复杂版
+ * 事不过三，复杂的时候使用reducer
+ * 例如在给表单赋值的时候可以用reducer
+ */
+
+const initial = {
+    n: 0
 }
-const render = () => {
-    index = 0 //这是最重要的一步!!!!!
-    ReactDOM.render(<App/>, document.getElementById('root'))
+const reducer = (state, action) => {
+    if (action.type === 'add') {
+        return {n: state.n + action.number}
+    } else if (action.type === 'multi') {
+        return {n: state.n * action.number}
+    } else {
+        throw new Error('unknown Type')
+    }
 }
 
 const App = () => {
-    const [n, setN] = myUseState(0)
-    const [m, setM] = myUseState(0)
+    const [state, dispatch] = useReducer(reducer, initial)
     const addOne = () => {
-        setN(n + 1)
+        dispatch({type: 'add', number: 1})
     }
-    const addOneM = () => {
-        setM(m + 1)
+    const addTwo = () => {
+        dispatch({type: 'add', number: 2})
     }
     return (
-        <>
-            <div>
-                <span>{n}</span>
-                <hr/>
-                <button onClick={addOne}>+1</button>
-            </div>
-            <div>
-                <span>{m}</span>
-                <hr/>
-                <button onClick={addOneM}>+1</button>
-            </div>
-        </>
+        <div>
+            <span>{state.n}</span>
+            <hr/>
+            <button onClick={addOne}>+1</button>
+            <button onClick={addTwo}>+2</button>
+        </div>
     )
 }
 export default App
