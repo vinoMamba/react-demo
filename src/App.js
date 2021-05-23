3905d62bd8ff8026fff43c7b165c715477f0e1d2
vino <vino0908@outlook.com>
2021-05-23 14:53:50 +0800

    实现自己的myUseState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, {useState} from "react";
-import useUpdate from "./useUpdate";
+import ReactDOM from 'react-dom';
 
+let _state
+const myUseState = (initialValue) => {
+    _state = _state === undefined ? initialValue : _state
+    const setState = (newValue) => {
+        _state = newValue
+        render()
+    }
+    return [_state, setState]
+}
+const render = () => {
+    ReactDOM.render(<App/>, document.getElementById('root'))
+}
 
-const App = (props) => {
-    const [n, setN] = useState(0)
+const App = () => {
+    const [n, setN] = myUseState(0)
     const addOne = () => {
         setN(n + 1)
     }
-    useUpdate(() => {
-        console.log('n变了')
-    }, n)
-
     return (
         <div>
             <span>{n}</span>
