c8f1be9f461c30a247cf26a9da8bd969b6d822c1
vino <vino0908@outlook.com>
2021-05-23 20:58:04 +0800

    useMemo and memo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React, {useEffect, useLayoutEffect, useState} from "react";
+import React, {memo, useCallback, useMemo, useState} from "react";
 
 
 const App = () => {
     const [n, setN] = useState(0)
+    const [m] = useState(0)
     const addOne = () => {
         setN(i => i + 1)
     }
-    useEffect(() => {
-        console.log('n 变化了')
-    }, [n])
-    useLayoutEffect(() => {
-        console.log('我在render 之前')
-    }, [n])
+    const onClickChild = useCallback(() => {
+        console.log(m)
+    }, [m])
+    /**
+     * 等价于下面的useMemo
+     */
+    // const onClickChild = useMemo(() => {
+    //     return () => {
+    //         console.log(m)
+    //     }
+    // }, [m])
     return (
         <div>
             <span>{n}</span>
             <hr/>
             <button onClick={addOne}>+1</button>
+            <hr/>
+            <Child data={m} onClick={onClickChild}/>
         </div>
     )
 }
+const Child = memo((props) => {
+    console.log('child 执行了')
+    console.log('假设这里有大量代码')
+    return (
+        <div onClick={props.onClick}>child: {props.data}</div>
+    )
+})
+
 export default App
